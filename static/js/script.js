42f3ad668573b1cc1c78a2758647af534a7b80e5
Samuelito660 <163178624+Samuelito660@users.noreply.github.com>
2024-10-21 16:54:36 +0200

    settima-bozza

--- static/js/script.js
+++ static/js/script.js
@@ -7,55 +7,69 @@ function generateRandomInteger(min = 0 , max=100){
     return intero
 }
 
-function Sanitize(n) {
-    sanitizeN =parseInt(n.trim());
+function Sanitize(numero) {
+    let sanitizeN = parseInt(numero.trim());
+
+    if (isNaN(sanitizeN)){
+        console.log("Valore non accettabile")
+    }
 
     return sanitizeN
 }
 
 
-        let ntentativi = 5
-        let NumDaInd = generateRandomInteger(min = 0 , max=100)
-        let vericita  = false
         
-        let text2 = document.querySelector(#text2)
-        let text3 = document.querySelector(#text3)
+function numerimagici(event){
 
-        let avvisi = document.querySelector("#guess").value
+    let avvisi = document.querySelector("#guess").value
 
+    num = Sanitize(avvisi) ;
 
-        numero = Sanitize(n) ;
+    if (num <= 0 || num > 100 || isNaN(num)) {
+        text2.innerHTML = 'Inserire un numero valido tra 1 e 100'
+        text3.innerHTML = 'Non hai inserito alcun numero'
 
-    if (numero <= 0 || numero > 100 || isNaN(numero)) {
-        avvisi.innerHTML = 'Inserire un numero valido tra 1 e 100'
-        return
+    }else if(num > NumDaInd){
+
+    ntentativi = ntentativi - 1 ;
+    text2.innerHTML = "Il numero <strong>" + num + "</strong> è più grande di quello dato!!";
+    text3.innerHTML = "Hai a disposizione ancora <strong>" + ntentativi + "</strong> tentativi";
+    vericita=false
+    
+    }else if(num < NumDaInd){
+
+    vericita=false ;
+    ntentativi = ntentativi - 1 ;
+    text2.innerHTML = "Il numero <strong>" + num + "</strong> è più piccolo di quello dato!!";
+    text3.innerHTML = "Hai a disposizione ancora <strong>" + ntentativi + "</strong> tentativi"
+
+    }else if(num === NumDaInd){
+
+    console.log("Hai  Indovinato!")
+    vericita=true
+    text2.innerHTML = 'Hai finalmente indovinato il numero'
+    text3.innerHTML = "Hai utilizato <strong>" + (6-ntentativi) + "</strong> tentativi"
     }
 
-    if(numero > NumDaInd){
+    if (ntentativi === 0 && !vericita) {
 
-    avvisi.innerHTML = 'Il numero da indovinare è più piccolo '
-    vericita=false
-    ntentativi = ntentativi - 1
+        text2.innerHTML = 'Hai esaurito i tentativi'
+        text3.innerHTML = "Il numero da indovinare era <strong>" + NumDaInd + "</strong>"
 
-    }else if(numero < NumDaInd){
-
-    avvisi.innerHTML = 'Il numero da indovinare è più grande'
-    vericita=false
-    ntentativi = ntentativi - 1
-
-    }else if(numero = NumDaInd){
-
-    avvisi.innerHTML = 'Hai indovinato !!! '
-    vericita=true
+    }
 
 }
 
+        
+    let ntentativi = 5
+    let NumDaInd = generateRandomInteger(min = 0 , max=100)
+    let num ;
+    let vericita  = false
+    let text2 = document.querySelector("#testo2")
+    let text3 = document.querySelector("#testo3")
 
-    document.querySelector("#guess").value = ""
+    
+    console.log("WEEEEELCOMEE TOOOOO NUMERI MAGICI")
 
-    if (ntentativi === 0) {
-
-        avvisi.innerHTML = ("Hai perso NABBO hai una L sulla fronte! Il numero da indovinare era: " + NumDaInd)
-
-    }
-    })
+    let btn = document.querySelector("#guessbtn")
+    btn.addEventListener("click", numerimagici)
